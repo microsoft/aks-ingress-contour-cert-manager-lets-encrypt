14af28ae77276224264c94ca4f6c47d71bb08b6a
Bart Robertson <bartr@microsoft.com>
2023-07-12 07:34:58 +0000

    updated Codespaces

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -56,7 +56,6 @@ vcluster completion zsh > "$HOME/.oh-my-zsh/completions/_vcluster"
 sudo apt-get update
 
 # only run apt upgrade on pre-build
-echo "CS: $CODESPACE_NAME" >> "$HOME/status"
 if [ "$CODESPACE_NAME" = "null" ] || [ "$CODESPACE_NAME" = "" ]
 then
     echo "$(date +'%Y-%m-%d %H:%M:%S')    upgrading" >> "$HOME/status"
