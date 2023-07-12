966c513920d3c65b3057bc55f1df988897e5d585
Bart Robertson <bartr@microsoft.com>
2023-07-12 07:33:31 +0000

    updated Codespaces

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -71,8 +71,7 @@ sudo az aks install-cli
 
 # install cert-mgr CLI
 curl -L -o kubectl-cert-manager.tar.gz https://github.com/jetstack/cert-manager/releases/latest/download/kubectl-cert_manager-linux-amd64.tar.gz
-tar xzf kubectl-cert-manager.tar.gz
-sudo mv kubectl-cert_manager /usr/local/bin
+sudo tar -xzf kubectl-cert-manager.tar.gz -C /usr/local/bin
 rm -f kubectl-cert-manager.tar.gz
 
 echo "on-create complete"
